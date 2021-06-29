0d8ae6645eb4d327a58e57c4892c1ce763293670
Dini Hassan <dinih51@gmail.com>
2021-06-29 11:56:09 +0100

    Calculator added

---
+++ app/Calculator/calc.py
@@ -0,0 +1,13 @@
+class Calculator:
+
+    def add(self, num1, num2):
+        return num1 + num2
+
+    def subtract(self, num1, num2):
+        return num1 - num2
+
+    def multi(self, value1, value2):
+        return value1 * value2
+
+    def divide(self, value1, value2):
+        return value1 / value2
